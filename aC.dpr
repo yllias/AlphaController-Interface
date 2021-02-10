a206c3f0cce2acc9ed3a75772197385389a10a87
Yll Kryeziu <5007185-yllkryeziu@users.noreply.gitlab.com>
2021-02-10 17:58:06 +0100

    fully functional v0.9

--- aC.dpr
+++ aC.dpr
@@ -22,7 +22,7 @@ type
     PUserData = ^TUserData;
 
     TUserData = record
-        ComPort, Baudrate, Mode: integer;
+        ComPort, Baudrate, Mode, Analog: integer;
     end;
 
     PParameterStruct = ^TParameterStruct;
